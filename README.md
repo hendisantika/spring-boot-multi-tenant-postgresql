c658d18a2c7ccce5c30c577f54c6bf7f916388fc
hendisantika <hendisantika@yahoo.co.id>
2025-06-17 06:58:16 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-multi-tenant-postgresql
---
+++ src/test/java/id/my/hendisantika/multitenantdemo5/MultitenantDemo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multitenantdemo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantDemo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
